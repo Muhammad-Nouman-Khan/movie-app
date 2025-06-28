223dea564b7be9b2148fe074e574db188d721d84
Muhammad-Nouman-Khan <nouman.younas.nyk@gmail.com>
2025-06-28 18:29:50 +0500

    feat: update Hero component to fetch and display dynamic movie data

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,16 +1,20 @@
 <template>
   <div
     class="w-full h-[400px] flex flex-col justify-center items-start px-8 relative gap-2"
-    style="
-      background-image: url('https://movies-proxy.vercel.app/ipx/f_webp&s_2440x1318/tmdb/fTrQsdMS2MUw00RnzH0r3JWHhts.jpg');
-      background-size: cover;
-      background-position: center;
+    :style="
+      heroMovie && heroMovie?.backdrop_path
+        ? {
+            backgroundImage: `url('https://image.tmdb.org/t/p/w780${heroMovie.backdrop_path}')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }
+        : {}
     "
   >
     <div
       class="absolute inset-0 bg-gradient-to-b from-black/0 to-black z-0"
     ></div>
-    <h1 class="text-4xl z-10 lg:text-6xl">A Working Man</h1>
+    <h1 class="text-4xl z-10 lg:text-6xl">{{ heroMovie?.title }}</h1>
     <div class="flex gap-2 items-center text-gray-400 z-10">
       <div class="flex">
         <Icon name="lucide:star" style="color: #00acac" />
@@ -20,19 +24,39 @@
         <Icon name="lucide:star" style="color: #00acac" />
       </div>
       <span>&bull;</span>
-      <span>2025</span>
+      <span>{{ heroMovie?.release_date }}</span>
       <span>&bull;</span>
       <span>1h 56min</span>
     </div>
     <p
       class="line-clamp-3 z-20 text-gray-300 text-xs md:text-base lg:w-[500px]"
     >
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
-      incidunt corrupti quae aperiam eum. Ut saepe officia error, quaerat
-      aliquam architecto veritatis ab nam? Expedita atque repellat, nam cum,
-      laborum eius quasi ipsa officiis, libero quaerat ut enim obcaecati rerum
-      assumenda inventore molestias ab ad ratione voluptates illum similique
-      ducimus.
+      {{ heroMovie?.overview }}
     </p>
   </div>
 </template>
+
+<script setup lang="ts">
+const { fetchFromTmdb } = useTmdb();
+
+type HeroMovie = {
+  backdrop_path: string | null;
+  title: string;
+  release_date: string;
+  overview: string;
+};
+
+const heroMovie = ref<HeroMovie | null>(null);
+
+const fetchHeroMovie = async () => {
+  try {
+    const res = await fetchFromTmdb("movie/popular");
+    heroMovie.value = res.results[0];
+  } catch (error) {
+    console.error("Error fetching hero movie:", error);
+  }
+};
+onMounted(() => {
+  fetchHeroMovie();
+});
+</script>
